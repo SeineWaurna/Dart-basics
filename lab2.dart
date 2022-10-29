7acef2bdde32b28da297293fb6903d5c632ca80a
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 13:47:10 +0700

    Create lab2.dart

---
+++ lab2.dart
@@ -0,0 +1,31 @@
+// เครื่องหมายทางคริตศาสตร์
+
+void main()
+{
+  int a = 2;
+  int b = 3;
+  
+  //Adding a and b
+  var c = a + b;
+  print("Sum of a and b is $c");
+  
+  //subtracting a and b
+  var d = a - b;
+  print("The different a and b is $d");
+  
+  //Using unary minus
+  var e = -d;
+  print("The negation of difference between a and b is $e");
+  
+  //Multiplication of a and b
+  var f = a * b;
+  print("The product of a and b is $f");
+  
+  //Division of a and b
+  var g = b / a;
+  print("The quotient of a and b is $g");
+  
+  //Rememeber of a and b
+  var i = b % a;
+  print("The remainder of a and b is $i");
+}
