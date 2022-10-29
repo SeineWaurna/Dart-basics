567e74d80e32a6871661bb233a928b8d4b4afc0c
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 14:38:23 +0700

    Create lab7.dart

---
+++ lab7.dart
@@ -0,0 +1,11 @@
+// Standard Input & Output
+
+import 'dart:io';
+
+void main() {
+  print("Please Enter your name : ");
+  String? name = stdin.readLineSync();
+  //String name;
+  //name = stdin.readLineSync();
+  print("Hello, $name\nWelcome to Thailand.");
+}
