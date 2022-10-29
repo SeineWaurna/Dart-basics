90341cdeddacf657e80f69e9e2e6407420f889af
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 13:34:57 +0700

    Create lab1.dart

---
+++ lab1.dart
@@ -0,0 +1,28 @@
+Basic Dart Programming
+- 
+// Single Line Comment
+
+/* Multiple Line comment
+ * Integer
+ * Double
+ * String
+ * Boolean
+ * Lists
+ * Maps
+ * 
+ * */
+
+void main() {
+  int age=15;
+  double salary = 34560.99;
+  bool married = false;
+  String name = "Elsa";
+  String title = "Developer";
+  
+  print(name);
+  print(title);
+  print(salary);
+  print(married);
+  print(age);
+
+}
