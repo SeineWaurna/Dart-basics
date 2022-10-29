5b9aa4fa641f87747bfe563db21d58312aec7f84
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 14:52:22 +0700

    Create lab8.dart

---
+++ lab8.dart
@@ -0,0 +1,20 @@
+// Number :int, double, num
+
+void main() {
+  
+  //declare an interger 
+  int num1 = 2;
+  
+  //declare a double value
+  double num2 = 1.5;
+  
+  //print the values
+  print(num1);
+  print(num2);
+  var a1 = num.parse("1");
+  var b1 = num.parse("2.34");
+  
+  var c1 = a1+b1;
+  print("Product = $c1");
+  print("Prodcut = ${c1}"); // Same as the code above
+}
