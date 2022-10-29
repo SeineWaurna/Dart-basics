0d13d34c70014456750cc75e36efed1511964dc7
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 14:58:50 +0700

    Create lab9.dart

---
+++ lab9.dart
@@ -0,0 +1,10 @@
+// String - Concatenation
+
+void main() {
+  
+  String string = 'Dart Programming';
+  String str = 'Coding is ';
+  String str1 = 'Fun';
+  print (string);
+  print (str + str1); //Concentration หรือการรวมค่า String เข้าด้วยกัน
+}
