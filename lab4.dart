e87d76036a8b397e7e2b43b63d8f890f8283d715
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 14:15:38 +0700

    Create lab4.dart

---
+++ lab4.dart
@@ -0,0 +1,15 @@
+// Type Test Operators
+// is เป็น
+// is! ไม่เป็น
+
+void main()
+{
+  String a = 'Peter';
+  double b = 3.39;
+  
+  //Using is to compare
+  print(a is String); // Is a String Data Type?
+  
+  //Using is! to compare
+  print(b is !int); // Is b ot an Integer?
+}
