1a511877dea4dd5617c5fd75844f160bab62068f
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 15:02:40 +0700

    Create lab10.dart

---
+++ lab10.dart
@@ -0,0 +1,14 @@
+// String - Concatenation
+// Comparison
+
+void main() {
+  
+  String string = 'Dart Programming';
+  String str = 'Coding is ';
+  String str1 = 'Fun';
+  print (string);
+  print (str + str1); //Concentration หรือการรวมค่า String เข้าด้วยกัน
+  
+  bool val = (str == str1); //String Comparison
+  print(val);
+}
