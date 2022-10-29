989c155f8412c6a6fd1eb63e2703f15e65baffe7
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-29 14:31:13 +0700

    Create lab6.dart

---
+++ lab6.dart
@@ -0,0 +1,16 @@
+// Using Conditional Operators in the program
+
+void main()
+{
+  int a = 5;
+  int b = 7;
+  
+  
+ //Conditional Statement
+  var c = (a < 10) ? "Statement is Correct" : "Statement is Wrong";
+  // if (a<10)
+  //    "Correct"
+  //  else
+  //    "Wrong"
+    print(c);
+}
