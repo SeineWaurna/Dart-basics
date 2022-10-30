eb507a532081fa12c9830832b46509c98878ede8
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-30 13:15:58 +0700

    Create lab12.dart

---
+++ lab12.dart
@@ -0,0 +1,14 @@
+// Map: Key and Value
+
+void main() {
+  Map m1 = new Map();
+  // m1[key] = "value"
+  m1['First'] = "AP Computer Science A";
+  m1['Second'] = "AP Computer Science Principle";
+  m1['Third'] = "Java programming" ;
+  print(m1);  // Print All of Map Data
+  print(m1['First']); // Print only key=First
+  print(m1['Second']);  // Print only key=Second
+  print(m1['Third']);  // Print only key=Thrid
+    
+}
