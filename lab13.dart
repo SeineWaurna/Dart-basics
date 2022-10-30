bed75c43c1d8f01867c343a11b4b2ebeb244dc6e
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-30 13:26:32 +0700

    Create lab13.dart

---
+++ lab13.dart
@@ -0,0 +1,17 @@
+// Switch Case
+// Check Condition
+
+void main() {
+  int num = 1;
+  switch (num) {
+    case 1: {
+      print("First");
+    }  break;
+    case 2: {
+      print("Second");
+    } break;
+    case 3: {
+      print("Third");
+    } break;
+  }
+}
