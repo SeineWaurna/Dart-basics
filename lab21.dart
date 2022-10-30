ed7e64782bf0a6db09b4e69ed10357f5d89f941b
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-10-30 14:50:28 +0700

    Create lab21.dart

---
+++ lab21.dart
@@ -0,0 +1,13 @@
+// Function with Optional Parameters or Optional Arguments
+// v2 is an optional argument
+// Optional Argument is in []
+
+void f1 (int v1, [int v2]) {
+  print("v1 is $v1");
+  print("v2 is $v2");
+}
+
+void main() {
+  print("Calling function with optional parameter");
+  f1(01);
+}
