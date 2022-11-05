7409ecf7013bdef60b2311208eec398c2e344387
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-11-05 13:41:47 +0700

    Create lab25.dart

---
+++ lab25.dart
@@ -0,0 +1,14 @@
+// Common Collection Functions
+// Collection = set of data and it can be 0 to many elements
+// in one variable
+
+void main() {
+  var x = []; // Empty Array
+  var y = ["Ant", "Bat", "Cat"];
+  
+  print(x.isEmpty); // true if there are no elements
+  print(x.isNotEmpty); // true if there are no elements
+  
+  print(x.isEmpty); // true if there are no elements
+  print(x.isNotEmpty); // true if there are no elements
+}
