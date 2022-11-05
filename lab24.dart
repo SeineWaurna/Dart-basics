1e4db01ddd36ca613e277a697246619b7755864d
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-11-05 13:31:23 +0700

    Create lab24.dart

---
+++ lab24.dart
@@ -0,0 +1,14 @@
+// Creat a variable as an array to store friends' name.
+// Create Anonymous function to print all elements of the variable
+
+void main() {
+  
+  var arrayFriends = ["Ant", "Black", "Ceaser", "Dino", "Eifel"];
+  
+  // arrow function
+ // arrayFriends.forEach((var arrayFriends) => print(arrayFriends));
+  
+  arrayFriends.forEach((i) {
+    print("$i");
+  });
+}
