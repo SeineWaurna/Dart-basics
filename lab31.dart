7b1dd39db1a73c35977628bb1995fbf90c4fa0df
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-11-05 15:05:27 +0700

    Create lab31.dart

---
+++ lab31.dart
@@ -0,0 +1,22 @@
+//Creating named constructor in Dart
+
+//Creating Class named Gfg
+class Gfg{
+
+// Creating named and paramaterized Constructor with one parameter
+  Gfg.constructor1(int a){
+    print('This is the parameterized constructor with only one parameter');
+  }
+  
+  //Creating named and prameterized Constructor with two parameter
+  Gfg.constructor2(int a, int b) {
+    print('This is the parameterized constructor with two parameters');
+    print('Value of a+b is ${a + b}');
+  }
+}
+
+void main() {
+  //Creating Instance of class
+  Gfg geek1 = new Gfg.constructor1(1); // One parameter Called
+  Gfg geek2 = new Gfg.constructor2(2, 3); // Two parameter Called
+}
