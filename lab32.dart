99d21dcbcee6b6b4bbf18db83d2e0e2f1b21f0ee
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-11-12 13:24:05 +0700

    Create lab32.dart

---
+++ lab32.dart
@@ -0,0 +1,12 @@
+// Function -> UDF (User Defind Function)
+// UDF -> Print Student Name -> Peter Parker
+// No Return Value
+// Result would be
+// My name is Peter Parker, I am a student.
+
+void name() => print("My name is Peter Parker, I am a student.");
+  
+void main()
+{
+    name();
+}
