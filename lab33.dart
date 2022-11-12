f4a073d40431e2a77e8e482180b4cb72769ab355
SeineWaurna <109640655+SeineWaurna@users.noreply.github.com>
2022-11-12 13:50:04 +0700

    Create lab33.dart

---
+++ lab33.dart
@@ -0,0 +1,19 @@
+// Function -> UDF (User Defind Function)
+// Write a functionn to find sum of two numbers
+// no return value function is required
+// name a function "additional"
+// Which one is a function to find sum of two numbers without return value
+// Parameterized Function with no return value
+// Void Parameterized Function
+
+void additional (int num1, int num2) {
+  int sum = num1 + num2;
+  print("The sum is $sum");
+}
+
+void main() {
+  additional(10, 200);
+}
+
+//Parameter -> additional (int num1, int num2) -> num1, num2
+// Argument -> additional(10, 200); -> 10, 200 -> Actual Value Passed
